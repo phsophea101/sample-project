dd88da86cf05d40a29f3716f6383459687416994
Sophea PHOS <52313314+phsophea101@users.noreply.github.com>
2023-12-22 16:35:52 +0700

    Delete src directory

--- src/main/java/com/sample/spring/service/ClientDetailService.java
+++
@@ -1,7 +0,0 @@
-package com.sample.spring.service;
-
-import com.sample.spring.dto.ClientDetailDto;
-
-public interface ClientDetailService {
-    ClientDetailDto findClientByClientId(String clientId);
-}
--- src/main/java/com/sample/spring/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.sample.spring.service;
-
-import com.sample.spring.dto.UserDto;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    UserDto save(UserDto dto);
-}
